3624104e13e2770f146759d99a1b51b0444ace7a
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2021-10-15 17:53:15 -0300

    Criando nova class e instanciando obj para teste

---
+++ ClassePessoa/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassePessoa
+{
+    class Pessoa
+    {
+        //atributos da class
+        public string nome;
+        public int idade;
+        public string rg;
+        public string cpf;
+
+        public void Acenar(Pessoa pessoa)
+        {
+            Console.WriteLine("{0} acenou para {1}", this.nome, pessoa.nome);
+        }
+
+        public void PerguntaIdade(Pessoa pessoa)
+        {
+            Console.WriteLine("{0} quantos anos você tem ?", pessoa.nome);
+            pessoa.RespostaIdade();
+        }
+
+        public void RespostaIdade()
+        {
+            Console.WriteLine("Eu tenho {0} anos", this.idade);
+        }
+    }
+}
---
+++ ClassePessoa/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ClassePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o nome:");
+            pessoa.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade:");
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu rg:");
+            pessoa.rg = Console.ReadLine();
+
+            Console.WriteLine("Digite seu CPF:");
+            pessoa.cpf = Console.ReadLine();
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("RG: {0}", pessoa.rg);
+            Console.WriteLine("CPF: {0}", pessoa.cpf);
+
+            Pessoa pessoaDiego = new Pessoa();
+            pessoaDiego.nome = "Diego Fernando";
+            pessoaDiego.idade = 33;
+            pessoaDiego.rg = "11.111.111-1";
+            pessoaDiego.cpf = "111.111.111-11";
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("RG: {0}", pessoa.rg);
+            Console.WriteLine("CPF: {0}", pessoa.cpf);
+
+            pessoa.Acenar(pessoaDiego);
+            pessoa.PerguntaIdade(pessoaDiego);
+        }
+    }
+}
